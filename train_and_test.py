ada2bcb904116a6151366e127cc2676edeb323e4
adamcsvarga <adamcs.varga@gmail.com>
2016-05-13 16:33:28 +0200

    final commit

--- train_and_test.py
+++ train_and_test.py
@@ -8,7 +8,7 @@ Created on Tue May 10 19:39:00 2016
 import pandas as pd, numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.cross_validation import KFold
-from sklearn import preprocessing
+from sklearn.feature_selection import SelectKBest, f_classif
 import quadratic_weighted_kappa as qwk
 import matplotlib.pyplot as plt
 
@@ -20,6 +20,23 @@ def train_and_xval(dataframe):
     
     
     predictors = ["lemmacount", "modifier2np", "avglen", "mispell"]
+    
+    # feature selection
+    selector = SelectKBest(f_classif, k=4)
+    selector.fit(dataframe[predictors], dataframe["domain1_score"])
+    scores = selector.pvalues_
+    scores[scores == 0] = 1e-319
+    scores = -np.log10(selector.pvalues_)
+    
+    plt.bar(range(len(predictors)), scores)
+    plt.xticks(range(len(predictors)), predictors, rotation='vertical')
+    plt.show()
+    plt.savefig('feas.png')
+    plt.close()
+
+
+    predictors = ["avglen", "mispell", "modifier2np", "lemmacount"]
+    
     alg = LinearRegression()
     kf = KFold(dataframe.shape[0], n_folds=10, random_state=1)
     
@@ -31,14 +48,16 @@ def train_and_xval(dataframe):
         test_predictions = alg.predict(dataframe[predictors].iloc[test,:])
         predictions.append(test_predictions)
         
-    predictions = np.concatenate(predictions, axis=0).round()
-        
+    predictions = np.concatenate(predictions, axis=0).round()   
+   
     fig, ax = plt.subplots()
     ax.scatter(dataframe["domain1_score"], predictions)
     ax.plot([dataframe["domain1_score"].min(), dataframe["domain1_score"].max()],[dataframe["domain1_score"].min(), dataframe["domain1_score"].max()], 'k--', lw=4)
     ax.set_xlabel('Expert score')
     ax.set_ylabel('Predicted score')    
-    plt.show()    
+    plt.show()
+    fig.savefig('regr.png')
+    plt.close(fig)
     
     return qwk.quadratic_weighted_kappa(dataframe["domain1_score"], predictions)
 
