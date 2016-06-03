fdf85ebffc0822f7a719623897d5571004ffc99c
adamcsvarga <adamcs.varga@gmail.com>
2016-06-03 11:00:01 +0200

    reading ease features

--- extract_fea.py
+++ extract_fea.py
@@ -2,11 +2,14 @@
 """
 Created on Tue May 10 17:59:25 2016
 
-@author: vurga
+v0.1 -- added readability score features (June 2 2016)
+
+@author: Adam Varga
 """
 
-from nltk.corpus import words
-import pandas as pd, lemma_count as lc, modifier_count as mc, spellcheck as sp
+from textstat.textstat import textstat
+import pandas as pd
+#import lemma_count as lc, modifier_count as mc, spellcheck as sp
 
 def read_data(filepath):
     return  pd.DataFrame.from_csv(filepath, sep="\t")
@@ -21,10 +24,23 @@ def wlen(essay):
 def create_features(dataframe):
     feas = dataframe[['essay_set','essay','domain1_score']].copy()
     
-    feas['lemmacount'] = feas['essay'].apply(lc.calculate)
+    # already computed
+    """feas['lemmacount'] = feas['essay'].apply(lc.calculate)
     feas['modifier2np'] = feas['essay'].apply(mc.calculate)
     feas['avglen'] = feas['essay'].apply(wlen)
-    feas['mispell'] = feas['essay'].apply(sp.check_spell)
+    feas['mispell'] = feas['essay'].apply(sp.check_spell)"""
+    
+    feas['flesch'] = feas['essay'].apply(textstat.flesch_reading_ease)
+    feas['smog'] = feas['essay'].apply(textstat.smog_index)
+    feas['fleschkinc'] = feas['essay'].apply(textstat.flesch_kincaid_grade)
+    feas['aread'] = feas['essay'].apply(textstat.automated_readability_index)
+    feas['dalechall'] = \
+				feas['essay'].apply(textstat.dale_chall_readability_score)
+    feas['diffwords'] = feas['essay'].apply(textstat.difficult_words)
+    feas['linwrite'] = feas['essay'].apply(textstat.linsear_write_formula)
+    feas['gunning'] = feas['essay'].apply(textstat.gunning_fog)
+    feas['textstand'] = feas['essay'].apply(textstat.text_standard)
+
     
     return feas
     
