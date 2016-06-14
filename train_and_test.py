55c3601f858c8f60b8593e35c2f0de2a6f50447e
adamcsvarga <adamcs.varga@gmail.com>
2016-06-14 19:47:24 +0200

    gradient boosting with all features

--- train_and_test.py
+++ train_and_test.py
@@ -8,9 +8,13 @@ Created on Tue May 10 19:39:00 2016
 import pandas as pd, numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.cross_validation import KFold
-from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.feature_selection import SelectKBest, f_classif, RFE, \
+VarianceThreshold
 import quadratic_weighted_kappa as qwk
 import matplotlib.pyplot as plt
+from sklearn.svm import SVR
+from sklearn.ensemble import BaggingRegressor, AdaBoostRegressor,\
+ ExtraTreesRegressor, GradientBoostingRegressor, RandomForestRegressor
 
 def read_data(filename):
     
@@ -19,9 +23,10 @@ def read_data(filename):
 def train_and_xval(dataframe):
     
     
-    to_excl = ["essay_set", "essay", "domain1_score"]  
+    to_excl = ["essay_set", "essay", "domain1_score"]
+    predictors = ["lemmacount", "modifier2np", "avglen", "mispell", "flesch", "smog", "fleschkinc", "aread", "dalechall", "diffwords", "linwrite", "gunning", "textstand"]
     predictors = dataframe.columns.difference(to_excl)
-
+    
     
     # feature selection
 #    selector = SelectKBest(f_classif, k=4)
@@ -37,21 +42,36 @@ def train_and_xval(dataframe):
 #    plt.close()
 
 
-    predictors = ["avglen", "mispell", "modifier2np", "lemmacount", "dalechall"]
+    #predictors = ["avglen", "mispell", "modifier2np", "lemmacount", "dalechall"]
     
-    alg = LinearRegression()
+    #alg = LinearRegression()
+    #alg = SVR(kernel='linear')
+    alg = GradientBoostingRegressor(max_depth=4,\
+    min_samples_leaf=3)
+    
+    # Drop sparse features
+    #sel = VarianceThreshold(threshold=(.9 * (1 - .9)))
+    #dn = pd.DataFrame(sel.fit_transform(dataframe[predictors]))
+    dn = dataframe[predictors]
+    
+    ## Feature ranking
+#    rfe = RFE(estimator=alg, n_features_to_select=1, step=1)
+#    rfe.fit(dataframe[predictors], dataframe["domain1_score"])
+#    ranking = rfe.ranking_
+#    print(ranking, len(predictors), rfe.n_features_, rfe.support_, rfe.estimator_)
+
+    
+    
     kf = KFold(dataframe.shape[0], n_folds=10, random_state=1)
     
     predictions = []
     for train, test in kf:
-        try:
-            train_predictors = (dataframe[predictors].iloc[train,:])
-            train_target = dataframe["domain1_score"].iloc[train]
-            alg.fit(train_predictors, train_target)
-            test_predictions = alg.predict(dataframe[predictors].iloc[test,:])
-            predictions.append(test_predictions)
-        except ValueError:
-            pass
+        train_predictors = (dn.iloc[train,:])
+        train_target = dataframe["domain1_score"].iloc[train]
+        alg.fit(train_predictors, train_target)
+        test_predictions = alg.predict(dn.iloc[test,:])
+        predictions.append(test_predictions)
+        
         
     predictions = np.concatenate(predictions, axis=0).round()   
    
@@ -67,6 +87,6 @@ def train_and_xval(dataframe):
     return qwk.quadratic_weighted_kappa(dataframe["domain1_score"], predictions)
 
 if __name__ == '__main__':
-    df = read_data('train_feas_full.csv')
-    print(train_and_xval(df[df.essay_set == 1]))
+    df = read_data('1_full_lsa.csv')
+    print(train_and_xval(df))
     
